711c1b09df475641a1d2a1e6e228588a623b7b12
Jachym Putta <jachym.putta@yale.edu>
2025-05-12 20:37:49 -0400

    chore: cleanup flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   outputs =
-    inputs@{
+    {
       self,
       nixpkgs,
       flake-utils,
@@ -22,35 +22,11 @@
           overlays = [ (import oxalica) ];
         };
 
-        rustWasmTarget = "wasm32-unknown-unknown";
         rustToolchain = with pkgs; [
           (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
         ];
-
-        # TODO: fix this package
-        wasmPkg = pkgs.rustPlatform.buildRustPackage {
-          pname = "raive";
-          src = self;
-          version = "0.1.0";
-          CARGO_BUILD_TARGET = rustWasmTarget;
-          RUSTFLAGS = "-C target-feature=+atomics,+bulk-memory,+mutable-globals";
-          buildInputs = with pkgs; [
-            wabt
-          ];
-          nativeBuildInputs = with pkgs; [
-            wabt
-          ];
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              # NOTE: Custom fork is not in crates
-              "gemini-rust-0.4.2" = "sha256-8ojKJpuNJKYLMhqPFm74gstVpt19FWknvt5iKspYZ7k=";
-            };
-          };
-        };
       in
       {
-        # Development environments
         devShell = pkgs.mkShell {
           RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
           shellHook = ''
@@ -58,14 +34,14 @@
           '';
           nativeBuildInputs = with pkgs; [
             pkg-config
-            rustToolchain # cargo, etc.
+            rustToolchain
             wasm-bindgen-cli
             wasm-pack
             just
+            nil
+            nixfmt-rfc-style
           ];
         };
-
-        defaultPackage = wasmPkg;
       }
     );
 }
